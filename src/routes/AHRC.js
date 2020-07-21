ef7588fa8c274f2bcd10172fd5b2c960cd50bdc2
Aaron Culp <aaron_culp@anfcorp.com>
2020-07-21 15:18:08 -0400

    updated some code, mostly css and removing spaces in html.

--- src/routes/AHRC.js
+++ src/routes/AHRC.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Nav from "../components/Nav";
 
 export default function AHRC() {
-  return (
-    <div className = 'ahrcContainer'>
-      NICK AHRC
-    </div>
-  )
+  return <div className="ahrcContainer">NICK AHRC</div>;
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,67 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Nav from '../components/Nav';
+import React from "react";
+import { Link } from "react-router-dom";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
-    <div className = 'page'>
-      <div className = 'homeContainer'>
-        <Nav/>
-        
-        <div className = 'projectsContainer'>
-          <div className = 'RickMorty'>
-          <Link to = '/RickAndMorty'>
-            <img 
-                src = 'https://res.cloudinary.com/aaronculp/image/upload/v1543943983/Portfolio/Run-the-Jewels-Rick-and-Morty.jpg' 
-                alt = 'Rick & Morty Space Adventures'
-                className = 'homeImage'
-            />
-          </Link>
-          </div>
+    <div className="page">
+      <div className="homeContainer">
+        <Nav />
 
-          <div className = 'Remedi'>
-          <Link to = '/Remedi'>
-            <img 
-                src = 'https://res.cloudinary.com/aaronculp/image/upload/v1543970506/Portfolio/RemediMobile.jpg' 
-                alt = 'Remedi'
-                className = 'homeImage'
-            />
-          </Link>
-          </div>
-
-          <div className = 'Portfolio'>
-          <Link to = '/Portfolio'>
-            <img 
-                src = 'https://res.cloudinary.com/aaronculp/image/upload/v1544029415/MAC.png' 
-                alt = 'Portfolio'
-                className = 'homeImage'
-            />
+        <div className="projectsContainer">
+          <div className="RickMorty">
+            <Link to="/RickAndMorty">
+              <img
+                src="https://res.cloudinary.com/aaronculp/image/upload/v1543943983/Portfolio/Run-the-Jewels-Rick-and-Morty.jpg"
+                alt="Rick & Morty Space Adventures"
+                className="homeImage"
+              />
             </Link>
           </div>
 
-          <div className = 'D17'>
-          <Link to = '/D17'>
-            <img 
-                src = 'https://res.cloudinary.com/aaronculp/image/upload/v1537200945/BAKERY-STYLE-CHOCOLATE-CHIP-COOKIES-9.jpg' 
-                alt = 'D17'
-                className = 'homeImage'              
-            />
+          <div className="Remedi">
+            <Link to="/Remedi">
+              <img
+                src="https://res.cloudinary.com/aaronculp/image/upload/v1543970506/Portfolio/RemediMobile.jpg"
+                alt="Remedi"
+                className="homeImage"
+              />
             </Link>
           </div>
 
-          <div className = 'AHRC'>
-          <Link to = '/AHRC'>
-            <img 
-                src = 'https://res.cloudinary.com/aaronculp/image/upload/v1543943983/Portfolio/Run-the-Jewels-Rick-and-Morty.jpg' 
-                alt = 'AHRC'
-                className = 'homeImage'
-            />
+          <div className="Portfolio">
+            <Link to="/Portfolio">
+              <img
+                src="https://res.cloudinary.com/aaronculp/image/upload/v1544029415/MAC.png"
+                alt="Portfolio"
+                className="homeImage"
+              />
             </Link>
           </div>
 
+          <div className="D17">
+            <Link to="/D17">
+              <img
+                src="https://res.cloudinary.com/aaronculp/image/upload/v1537200945/BAKERY-STYLE-CHOCOLATE-CHIP-COOKIES-9.jpg"
+                alt="D17"
+                className="homeImage"
+              />
+            </Link>
+          </div>
 
+          <div className="AHRC">
+            <Link to="/AHRC">
+              <img
+                src="https://res.cloudinary.com/aaronculp/image/upload/v1543943983/Portfolio/Run-the-Jewels-Rick-and-Morty.jpg"
+                alt="AHRC"
+                className="homeImage"
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
